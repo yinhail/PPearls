f920299671a50ebca3d75142ceab52879b067043
Ray Lu <yinhail@gmail.com>
2014-04-17 13:55:12 +0800

    Add shift func in chatper2

---
+++ PPearls/PPearls/Chapter2.cs
@@ -0,0 +1,117 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Chapter2.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   The chapter 2.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace PPearls
+{
+    using System;
+
+    /// <summary>
+    /// The chapter 2.
+    /// </summary>
+    internal class Chapter2
+    {
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// Shift the character in 
+        /// <seealso cref="str"/>
+        /// to the right by n.
+        /// </summary>
+        /// <param name="str">
+        /// string
+        /// </param>
+        /// <param name="n">
+        /// offset.
+        /// </param>
+        public static void shift(char[] str, int n)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+
+            int length = str.Length;
+            if (n > length)
+            {
+                n = n % length;
+            }
+
+            if (n < 0)
+            {
+                n = n % length + length;
+            }
+
+            int gcd = Gcd(length, n);
+
+            for (int ii = 0; ii < gcd; ii++)
+            {
+                int jj = ii;
+                char tmp = str[jj];
+                for (int cc = 0; cc < length / gcd; cc++)
+                {
+                    var nextTmp = str[next(length, n, jj)];
+                    str[next(length, n, jj)] = tmp;
+                    tmp = nextTmp;
+                    jj = next(length, n, jj);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// The gcd.
+        /// </summary>
+        /// <param name="a">
+        /// The a.
+        /// </param>
+        /// <param name="b">
+        /// The b.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int"/>.
+        /// </returns>
+        private static int Gcd(int a, int b)
+        {
+            if (a == 0)
+                throw  new InvalidOperationException("a");
+            if (b == 0)
+                throw  new InvalidOperationException("b");
+            if (a < b)
+            {
+                return Gcd(b, a);
+            }
+
+            return a % b == 0 ? b : Gcd(b, a % b);
+        }
+
+        /// <summary>
+        /// The next.
+        /// </summary>
+        /// <param name="length">
+        /// The length.
+        /// </param>
+        /// <param name="n">
+        /// The n.
+        /// </param>
+        /// <param name="currentIndex">
+        /// The current index.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int"/>.
+        /// </returns>
+        private static int next(int length, int n, int currentIndex)
+        {
+            return (currentIndex + n) % length;
+        }
+
+        #endregion
+    }
+}
---
+++ PPearls/PPearlsTests/Chapter2Test.cs
@@ -0,0 +1,144 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Chapter2Test.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   This is a test class for Chapter2Test and is intended
+//   to contain all Chapter2Test Unit Tests
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace PPearlsTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using PPearls;
+
+    /// <summary>
+    /// This is a test class for Chapter2Test and is intended
+    /// to contain all Chapter2Test Unit Tests
+    /// </summary>
+    [TestClass]
+    public class Chapter2Test
+    {
+        #region Fields
+
+        /// <summary>
+        /// The test context instance.
+        /// </summary>
+        private TestContext testContextInstance;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the test context which provides
+        /// information about and functionality for the current test run.
+        /// </summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return this.testContextInstance;
+            }
+
+            set
+            {
+                this.testContextInstance = value;
+            }
+        }
+
+        #endregion
+
+        // You can use the following additional attributes as you write your tests:
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext)
+        // {
+        // }
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup()
+        // {
+        // }
+        // Use TestInitialize to run code before running each test
+        // [TestInitialize()]
+        // public void MyTestInitialize()
+        // {
+        // }
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup()
+        // {
+        // }
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The not primer.
+        /// </summary>
+        [TestMethod]
+        public void NotPrimer()
+        {
+            char[] str = new[] { 'a', 'b', 'c', 'd' };
+
+            Chapter2.shift(str, 2);
+
+            Assert.AreEqual("cdab", new string(str));
+        }
+
+        /// <summary>
+        /// A test for shift
+        /// </summary>
+        [TestMethod]
+        public void Primer()
+        {
+            char[] str = new[] { 'a', 'b', 'c', 'd', 'e' };
+
+            Chapter2.shift(str, 3);
+
+            Assert.AreEqual("cdeab", new string(str));
+        }
+
+        /// <summary>
+        /// The shift by bigger than length.
+        /// </summary>
+        [TestMethod]
+        public void ShiftByBiggerThanLength()
+        {
+            char[] str = new[] { 'a', 'b', 'c', 'd', 'e' };
+
+            Chapter2.shift(str, 37);
+
+            Assert.AreEqual("deabc", new string(str));
+        }
+
+        /// <summary>
+        /// The shift by negative.
+        /// </summary>
+        [TestMethod]
+        public void ShiftByNegative()
+        {
+            char[] str = new[] { 'a', 'b', 'c', 'd' };
+
+            Chapter2.shift(str, -7);
+
+            Assert.AreEqual("dabc", new string(str));
+        }
+
+        /// <summary>
+        /// The shift by zero.
+        /// </summary>
+        [TestMethod]
+        public void ShiftByZero()
+        {
+            char[] str = new[] { 'a', 'b', 'c', 'd' };
+
+            Chapter2.shift(str, 0);
+
+            Assert.AreEqual("abcd", new string(str));
+        }
+
+        #endregion
+    }
+}
